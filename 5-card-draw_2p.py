fe15fb2121d023c7ee5aa7dd03f201a2dc91a1a6
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2024-07-09 10:07:46 -0400

    Update 5-card-draw_2p.py

--- 5-card-draw_2p.py
+++ 5-card-draw_2p.py
@@ -189,7 +189,7 @@ class oddsTable:
     numBorA = (ord(BorA.lower())-97)*2
     if debug:
       print("numBorA = "+str(numBorA))
-      print("self.handsTable["+hand+"] = "+string(self.handsTable[hand]))
+      print("self.handsTable["+hand+"] = "+str(self.handsTable[hand]))
       print("self.handsTable["+hand+"]["+str(numBorA)+"] = "+str(self.handsTable[hand][numBorA]))
       print("self.handsTable["+hand+"]["+str(numBorA+1)+"] = "+str(self.handsTable[hand][numBorA+1]))
     answer = 1.0 * self.handsTable[hand][numBorA+1] / self.handsTable[hand][numBorA]
